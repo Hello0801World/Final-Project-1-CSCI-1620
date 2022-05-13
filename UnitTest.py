ea9a445a78ca86275405a7d901cfe1420a38b43f
Shun <shun4068841@gmail.com>
2022-05-13 15:33:09 +0900

    Modified unit test

--- UnitTest.py
+++ UnitTest.py
@@ -4,21 +4,50 @@ from classes import *
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.tv1=Television(0,0)
-        self.tv2=Television(5,5)
+        self.channel_value=1
+        self.volume_value=0
+        self.tv = Television(self.channel_value,self.volume_value)
 
-    def tearDown(self) -> None:
-        del self.tv1
-        del self.tv2
 
-    def test_init(self):
-        self.assertEqual(self.tv1.channel_up(),1)
-        self.assertEqual(self.tv2.channel_up(),0)
+    #def tearDown(self) -> None:
+        #pass
 
-    #def test_channel_up(self):
-        #self.tv1.channel_up()
-        #self.assertEqual(self.tv1.channel_up(), 1)
+    def test_channel_up(self):
+        self.tv.channel_up()
+        self.assertEqual(self.tv.channel_up(), 3)
+        self.tv.channel_up()
+        self.assertEqual(self.tv.channel_up(), 1)
 
+
+    def test_channel_down(self):
+        self.channel_value=4
+        self.tv=Television(self.channel_value,self.volume_value)
+        self.assertEqual(self.tv.channel_down(),3)
+        self.tv.channel_down()
+        self.assertEqual(self.tv.channel_down(), 1)
+
+
+
+    def test_volume_up(self):
+        self.tv.volume_up()
+        self.assertEqual(self.tv.volume_up(),2)
+        self.tv.volume_up()
+        self.tv.volume_up()
+        self.tv.volume_up()
+        self.assertEqual(self.tv.volume_up(), 5)
+
+
+
+    def test_volume_down(self):
+        self.volume_value=5
+        self.tv = Television(self.channel_value,self.volume_value)
+        self.tv.volume_down()
+        self.assertEqual(self.tv.volume_down(),3)
+        self.tv.volume_down()
+        self.tv.volume_down()
+        self.tv.volume_down()
+        self.tv.volume_down()
+        self.assertEqual(self.tv.volume_down(), 0)
 
 
 
